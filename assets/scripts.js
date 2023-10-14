5f63e7326c0a6cdce919b0f43ffd4f7fc9101157
Dsczin <carvalhocaua039@gmail.com>
2023-10-14 17:32:55 -0300

    traducao

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,15 @@
 const key = "fcc8d9af9001877d56b1ee3966781e94"
 
+function colocarDadosNaTela(dados) {
+
+    console.log(dados)
+
+}
 
 async function buscarCidade(cidade){     
     
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json( ) ) 
-    console.log(dados) 
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then( resposta => resposta.json( ) ) 
+    colocarDadosNaTela(dados) 
 }
 
    
